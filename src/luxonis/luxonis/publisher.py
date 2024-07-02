ffaf7b9fe9b9a467ad4ad8d3b46c9296f5346726
Polo <leonardopolo266@gmail.com>
2024-07-02 10:01:29 +0200

    config file implementation

--- src/luxonis/luxonis/publisher.py
+++ src/luxonis/luxonis/publisher.py
@@ -8,7 +8,14 @@ from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
 from datetime import timedelta
 
+# Default values
 RESIZE_RESOLUTION = (640, 480)
+SPECKLE_FILTER = True
+COLOR_CAMERA_RES = dai.ColorCameraProperties.SensorResolution.THE_1080_P
+MONO_CAMERA_RES = dai.MonoCameraProperties.SensorResolution.THE_720_P
+COLOR_CAMERA_FPS = 40
+MONO_CAMERA_FPS = 40
+SYNC_THRESHOLD_SECONDS = 0.2
 
 EXTENDED_DISPARITY = False
 SUBPIXEL = False
@@ -27,16 +34,91 @@ DEPTH_TOPIC_NAME = "depth"
 CAMERA_INFO_TOPIC_NAME = "cameraInfo"
 ACTION_TOPIC_NAME = "takePhoto"
 
-calibData = None
-currentTime = None
+NODE_NAME = "camera"
 
 def flatten_matrix(matrix):
     return [item for sublist in matrix for item in sublist]
 
+def parseConfig():
+    try:
+        config = open("config.json", "r")
+    except:
+        print("Cannot load config.json")
+        exit(-1)
+
+    configData = json.load(config)
+
+    global RESIZE_RESOLUTION_WIDTH, RESIZE_RESOLUTION_HEIGHT, COLOR_CAMERA_RES
+    global MONO_CAMERA_RES, COLOR_CAMERA_FPS, MONO_CAMERA_FPS, SPECKLE_FILTER
+    global THRESHOLD, BILATERAL_SIGMA, LRCHECK, EXTENDED_DISPARITY, SUBPIXEL
+    global SYNC_THRESHOLD_SECONDS, NODE_NAME, RGB_TOPIC_NAME, DEPTH_TOPIC_NAME
+    global CAMERA_INFO_TOPIC_NAME, ACTION_TOPIC_NAME
+
+    if configData["resizeResolution"]["width"]:
+        RESIZE_RESOLUTION_WIDTH = configData["resizeResolution"]["width"]
+    
+    if configData["resizeResolution"]["width"]:
+        RESIZE_RESOLUTION_HEIGHT = configData["resizeResolution"]["height"]
+
+    if configData["camera"]["rgb"]["resolution"]:
+        if configData["camera"]["rgb"]["resolution"] == "1080":
+            COLOR_CAMERA_RES = dai.ColorCameraProperties.SensorResolution.THE_1080_P
+        elif configData["camera"]["rgb"]["resolution"] == "4K":
+            COLOR_CAMERA_RES = dai.ColorCameraProperties.SensorResolution.THE_4_K
+    
+    if configData["camera"]["mono"]["resolution"]:
+        if configData["camera"]["mono"]["resolution"] == "720":
+            MONO_CAMERA_RES = dai.MonoCameraProperties.SensorResolution.THE_720_P
+        elif configData["camera"]["mono"]["resolution"] == "400":
+            MONO_CAMERA_RES = dai.MonoCameraProperties.SensorResolution.THE_400_P
+    
+    if configData["camera"]["rgb"]["fpsCap"]:
+        COLOR_CAMERA_FPS = configData["camera"]["rgb"]["fpsCap"]
+    
+    if configData["camera"]["rgb"]["fpsCap"]:
+        MONO_CAMERA_FPS = configData["camera"]["mono"]["fpsCap"]
+
+    if configData["stereo"]["speckleFilter"]:
+        SPECKLE_FILTER = configData["stereo"]["speckleFilter"]
+    
+    if configData["stereo"]["confidenceThreshold"]:
+        THRESHOLD = configData["stereo"]["confidenceThreshold"]
+
+    if configData["stereo"]["bilateralSigma"]:
+        BILATERAL_SIGMA = configData["stereo"]["bilateralSigma"]
+    
+    if configData["stereo"]["LRCheck"]:
+        LRCHECK = configData["stereo"]["LRCheck"]
+    
+    if configData["stereo"]["extendedDisparity"]:
+        EXTENDED_DISPARITY = configData["stereo"]["extendedDisparity"]
+    
+    if configData["stereo"]["subpixel"]:
+        SUBPIXEL = configData["stereo"]["subpixel"]
+
+    if configData["sync"]["syncThresholdSeconds"]:
+        SYNC_THRESHOLD_SECONDS = configData["sync"]["syncThresholdSeconds"]
+    
+    if configData["node"]["name"]:
+        NODE_NAME = configData["node"]["name"]
+    
+    if configData["node"]["rgbTopicName"]:
+        RGB_TOPIC_NAME = configData["node"]["rgbTopicName"]
+    
+    if configData["node"]["depthTopicName"]:
+        DEPTH_TOPIC_NAME = configData["node"]["depthTopicName"]
+    
+    if configData["node"]["cameraInfoTopicName"]:
+        CAMERA_INFO_TOPIC_NAME = configData["node"]["cameraInfoTopicName"]
+
+    if configData["node"]["actionTopicName"]:
+        ACTION_TOPIC_NAME = configData["node"]["actionTopicName"]
+
+
 class Camera(Node):
 
     def __init__(self, qSync, qControl):
-        super().__init__('camera')
+        super().__init__(NODE_NAME)
         self.qSync = qSync
         self.qControl = qControl
         self.i = 0
@@ -102,7 +184,7 @@ class Camera(Node):
 
         # Converting to ROS format and sending it
         try:
-            cv_image = cv2.resize(cv_image, RESIZE_RESOLUTION, interpolation=cv2.INTER_LINEAR)
+            cv_image = cv2.resize(cv_image, (RESIZE_RESOLUTION_WIDTH, RESIZE_RESOLUTION_HEIGHT), interpolation=cv2.INTER_LINEAR)
             image_ROS = self.bridge.cv2_to_imgmsg(cv_image)
             image_ROS.header.stamp = currentTime
             print("OpenCV image converted to ROS format")
@@ -122,7 +204,7 @@ class Camera(Node):
 
         # Converting to ROS format and sending it
         try:
-            cv_image = cv2.resize(cv_image, RESIZE_RESOLUTION, interpolation=cv2.INTER_LINEAR)
+            cv_image = cv2.resize(cv_image, (RESIZE_RESOLUTION_WIDTH, RESIZE_RESOLUTION_HEIGHT), interpolation=cv2.INTER_LINEAR)
             image_ROS = self.bridge.cv2_to_imgmsg(cv_image)
             image_ROS.header.stamp = currentTime
             print("OpenCV image converted to ROS format")
@@ -138,13 +220,13 @@ class Camera(Node):
         camera_info_msg = CameraInfo()
 
         camera_info_msg.header.stamp = currentTime
-        camera_info_msg.height = RESIZE_RESOLUTION[1]
-        camera_info_msg.width = RESIZE_RESOLUTION[0]
+        camera_info_msg.height = RESIZE_RESOLUTION_HEIGHT
+        camera_info_msg.width = RESIZE_RESOLUTION_WIDTH
         
         camera_info_msg.k = flatten_matrix(calibData.getCameraIntrinsics(
             dai.CameraBoardSocket.CAM_A,
-            resizeWidth=RESIZE_RESOLUTION[0],
-            resizeHeight=RESIZE_RESOLUTION[1]))
+            resizeWidth=RESIZE_RESOLUTION_WIDTH,
+            resizeHeight=RESIZE_RESOLUTION_HEIGHT))
 
         self.publisherCameraInfo.publish(camera_info_msg)
         print("Camera info sent")
@@ -163,7 +245,8 @@ def main(args=None):
     camRgb = pipeline.create(dai.node.ColorCamera)
     camRgb.setBoardSocket(dai.CameraBoardSocket.CAM_A)
     camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
-    camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+    camRgb.setResolution(COLOR_CAMERA_RES)
+    camRgb.setFps(COLOR_CAMERA_FPS)
 
     # Connect the RGB stream to the sync node
     camRgb.still.link(sync.inputs[STILL_STREAM_NAME])
@@ -179,15 +262,17 @@ def main(args=None):
     monoRight = pipeline.create(dai.node.MonoCamera)
     stereo = pipeline.create(dai.node.StereoDepth)
     
-    monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
-    monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+    monoRight.setResolution(MONO_CAMERA_RES)
+    monoLeft.setResolution(MONO_CAMERA_RES)
+    monoRight.setFps(MONO_CAMERA_FPS)
+    monoLeft.setFps(MONO_CAMERA_FPS)
     monoRight.setBoardSocket(dai.CameraBoardSocket.CAM_C)
     monoLeft.setBoardSocket(dai.CameraBoardSocket.CAM_B)
 
     monoLeft.out.link(stereo.left)
     monoRight.out.link(stereo.right)
 
-    stereo.initialConfig.PostProcessing.SpeckleFilter.enable = True
+    stereo.initialConfig.PostProcessing.SpeckleFilter.enable = SPECKLE_FILTER
     stereo.initialConfig.setConfidenceThreshold(THRESHOLD)
     stereo.initialConfig.setBilateralFilterSigma(BILATERAL_SIGMA)
     stereo.setLeftRightCheck(LRCHECK)
@@ -201,7 +286,7 @@ def main(args=None):
     stereo.disparity.link(sync.inputs[DEPTH_STREAM_NAME])
 
     # 2 frames are considered synced if shoot in a window of 0.5 seconds
-    sync.setSyncThreshold(timedelta(seconds = 0.2))
+    sync.setSyncThreshold(timedelta(seconds = SYNC_THRESHOLD_SECONDS))
     
     xoutSynced = pipeline.create(dai.node.XLinkOut)
     xoutSynced.setStreamName(SYNC_STREAM_NAME)
